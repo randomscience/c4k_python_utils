dfe3e4a2e26fbe2ac0b1cd380c7ea02ba6cb2128
Mateusz Kojro <mateuszkojro@outlook.com>
2023-09-10 23:40:59 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 metadata = dict(
     name="c4k_python_utils",
-    version="0.1.6",
+    version="0.1.7",
     packages=["c4k_python_utils"],
     install_requires=[
         "numpy",
