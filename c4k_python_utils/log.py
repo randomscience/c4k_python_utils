4d1133fa4c82d803a34aaeab78801e0a26a1d4c3
Mateusz Kojro <mateuszkojro@outlook.com>
2023-11-11 21:08:54 +0100

    More sensible implementation of logging

--- c4k_python_utils/log.py
+++ c4k_python_utils/log.py
@@ -1,17 +1,9 @@
 import logging
-from abc import ABC, abstractmethod
-import unittest
 import smtplib
 import ssl
 
 
-class Notifier(ABC):
-    @abstractmethod
-    def notify(self, message: str, level_name: str, app_name: str):
-        pass
-
-
-class EmailNotifier(Notifier):
+class EmailLogHandler(logging.Handler):
     @staticmethod
     def _send_mail(
         receiver,
@@ -34,94 +26,33 @@ class EmailNotifier(Notifier):
         receivers,
         sender_email,
         password,
+        notify_level=logging.ERROR,
         smtp_server="smtp.gmail.com",
         smtp_port=465,
     ) -> None:
+        super().__init__()
         self._receivers = receivers
         self._sender_email = sender_email
         self._password = password
+        self._notify_level = notify_level
         self._smtp_server = smtp_server
         self._smtp_port = smtp_port
 
-    def notify(self, message, level_name, app_name):
-        for receiver in self._receivers:
-            EmailNotifier.send_mail(receiver, f"[{app_name}]] {level_name}", message)
+    def handleError(self, record: logging.LogRecord) -> None:
+        return super().handleError(record)
 
+    def emit(self, record: logging.LogRecord):
+        try:
+            if record.levelno < self._notify_level:
+                return
 
-class Logger:
-    def __init__(
-        self,
-        app_name,
-        notify_level=logging.ERROR,
-        notifiers: list[Notifier] = None,
-        logger: logging.Logger = None,
-    ) -> None:
-        self._app_name = app_name
-        self._notify_level = notify_level
-        self._notifiers = [] if notifiers is None else notifiers
-        self._logger = (
-            logging.Logger(app_name, logging.INFO) if logger is None else logger
-        )
-
-    def log(self, message, level):
-        self._logger.log(level, message)
-
-        if level >= self._notify_level:
-            for notifier in self._notifiers:
-                try:
-                    notifier.notify(
-                        message, logging.getLevelName(level), self._app_name
-                    )
-                except Exception as e:
-                    self._logger.critical(
-                        f"Could not notify with '{notifier.__class__.__name__}': {e}"
-                    )
-
-    def info(self, message):
-        self.log(message, logging.INFO)
-
-    def error(self, message):
-        self.log(message, logging.ERROR)
-
-
-class Test(unittest.TestCase):
-    class ThrowingNotifier(Notifier):
-        def notify(self, message: str, level_name: str, app_name: str):
-            raise RuntimeError("Exception message")
-
-    class DebugNotifier(Notifier):
-        def __init__(self) -> None:
-            self.notifications = []
-
-        def notify(self, message: str, level_name: str, app_name: str):
-            self.notifications.append(message)
-
-    def setUp(self) -> None:
-        logging.basicConfig(level=logging.INFO)
-
-    def test_never_crash(self):
-        logger = Logger("app", notifiers=[self.ThrowingNotifier()])
-        logger.error("aa")
-
-    def test_send_notification_when_appropriate(self):
-        debug_notifier = self.DebugNotifier()
-        logger = Logger("app", notify_level=logging.INFO, notifiers=[debug_notifier])
-
-        log_message = "log"
-        logger.info(log_message)
-        self.assertIn(log_message, debug_notifier.notifications)
-
-        error_message = "error"
-        logger.error(error_message)
-        self.assertIn(error_message, debug_notifier.notifications)
-
-    def test_dont_send_notification_when_not_appropriate(self):
-        debug_notifier = self.DebugNotifier()
-        logger = Logger("app", notify_level=logging.ERROR, notifiers=[debug_notifier])
-        message = "aaa"
-        logger.info(message)
-        self.assertNotIn(message, debug_notifier.notifications)
-
-
-if __name__ == "__main__":
-    unittest.main()
+            for receiver in self._receivers:
+                EmailLogHandler.send_mail(
+                    receiver,
+                    f"[{record.name}]] {record.levelname}",
+                    self.format(record),
+                )
+        except RecursionError:  # See issue 36272
+            raise
+        except Exception:
+            self.handleError(record)
